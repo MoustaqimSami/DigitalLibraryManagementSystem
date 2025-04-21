47953483e16092c502b58840a390703ff3e71052
moust <mohammad.moustaqi1@ucalgary.ca>
2025-04-21 14:17:42 -0600

    Changes made for animating form

--- Login/StaffEnd/addEdit/addEditScreen.js
+++ Login/StaffEnd/addEdit/addEditScreen.js
@@ -4,3 +4,12 @@ const sidebar = document.querySelector(".sidebar");
 toggleBtn.addEventListener("click", () => {
   sidebar.classList.toggle("collapsed");
 });
+
+const createDocBtn = document.getElementById("createDocBtn");
+const docWrapper = document.querySelector(".doc-center-wrapper");
+const form = document.getElementById("createDocForm");
+
+createDocBtn.addEventListener("click", () => {
+  docWrapper.classList.add("creating-document");
+  form.classList.remove("hidden");
+});
