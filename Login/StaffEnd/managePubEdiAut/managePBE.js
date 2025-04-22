143fc974218033b741caf4b432924b42dfbb0a03
moust <mohammad.moustaqi1@ucalgary.ca>
2025-04-22 13:50:44 -0600

    fixed PBE Dashboard

---
+++ Login/StaffEnd/managePubEdiAut/managePBE.js
@@ -0,0 +1,6 @@
+// Sidebar toggle
+const toggleBtn = document.getElementById("toggleSidebar");
+const sidebar = document.querySelector(".sidebar");
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("collapsed");
+});
